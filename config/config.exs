b518e102c8403ee3b1b841608acfa9b01846ba85
Gabrielzyr <gabrielmz397@gmail.com>
2021-02-26 11:04:36 -0300

    Added User migration and schemas

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,10 @@ config :nlwrocketpay, NlwrocketpayWeb.Endpoint,
   pubsub_server: Nlwrocketpay.PubSub,
   live_view: [signing_salt: "GPizNfu2"]
 
+config :nlwrocketpay, Nlwrocketpay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
